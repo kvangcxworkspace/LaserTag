9b1d044dc453d9d37215dc84530cc708a6b57bd2
Kue <kvangcx@outlook.com>
2018-01-27 15:46:50 -0500

    Change Server Code
    Change the Server Code to allow for mutl-threading and added one-to-many server-client addition.

---
+++ LaserTag/Program.cs
@@ -0,0 +1,22 @@
+﻿using LaserTagServer;
+using System;
+using System.Threading;
+
+namespace LaserTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Thread serverThread = new Thread(new ThreadStart(RunServer));
+
+            serverThread.Start();
+        }
+
+        public static void RunServer()
+        {
+            Server server = new Server(8000);
+            server.Run();
+        }
+    }
+}
